4aa2a3618a20e22d19b84322fc253ce12e793b42
Derek De young <derekdeyoung@gmail.com>
2015-11-17 15:05:50 -0500

    added stuff

---
+++ TableCode/reedBoard.py
@@ -0,0 +1,16 @@
+class ReedBoard:
+
+	def __init__(self, Pin):
+		self.pin = Pin # replace direction with pin info
+		self.currentBoard = [[0 for x in range(12)] for y in range(8)]
+		
+		
+		
+	def testingInput(self, testArray):
+		self.currentBoard = testArray
+
+	def updateBoard():
+		self.currentBoard = self.currentBoard = [[0 for x in range(12)] for y in range(8)]
+
+	def getBoard():
+		return self.currentBoard
